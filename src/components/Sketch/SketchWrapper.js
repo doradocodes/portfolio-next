85ee7e84126c0adfc29bf897fbfc784a8c7bbc2e
Dora <doradocodes@gmail.com>
2024-07-17 12:21:27 -0400

    Remove sketch source code for now

--- src/components/Sketch/SketchWrapper.js
+++ src/components/Sketch/SketchWrapper.js
@@ -7,13 +7,13 @@ import { sketch1 } from "@/components/Sketch/sketch";
 
 export default function SketchWrapper() {
     return <div className={styles.wrapper}>
-        <div className={styles.source}>
-            <pre>
-                <code className={styles.code} suppressHydrationWarning>
-                    {sketch1.toString()}
-                </code>
-            </pre>
-        </div>
+        {/*<div className={styles.source}>*/}
+        {/*    <pre>*/}
+        {/*        <code className={styles.code} suppressHydrationWarning>*/}
+        {/*            {`${sketch1.toString()}`}*/}
+        {/*        </code>*/}
+        {/*    </pre>*/}
+        {/*</div>*/}
         <NextReactP5Wrapper sketch={sketch1} />
     </div>;
 }
