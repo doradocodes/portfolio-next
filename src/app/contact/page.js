15e75ebb6ebebccf6459bceb6f7f3d62addb00dc
Dora <doradocodes@gmail.com>
2024-12-28 17:20:52 -0800

    Add contact form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function ContactPage() {
     return <div className="page">
         <h2>Contact me</h2>
-        <div className="content">
+        <div className={styles.content}>
             <p>
                 Instagram: <a target="_blank" href="https://www.instagram.com/doraymee/?hl=en">@doraymee</a>
             </p>
@@ -15,20 +15,24 @@ export default function ContactPage() {
             </p>
         </div>
 
-        {/*<form className={styles.form} netlify>*/}
-        {/*    <label>*/}
-        {/*        Name*/}
-        {/*        <input type="text" name="name" required/>*/}
-        {/*    </label>*/}
-        {/*    <label>*/}
-        {/*        Email*/}
-        {/*        <input type="email" name="email" required/>*/}
-        {/*    </label>*/}
-        {/*    <label>*/}
-        {/*        Message*/}
-        {/*        <textarea name="message" required/>*/}
-        {/*    </label>*/}
-        {/*    <button type="submit">Send</button>*/}
-        {/*</form>*/}
+        <form className={styles.form} netlify>
+            <div className={styles.formRow}>
+                <label>Name</label>
+                <input type="text" name="name" placeholder="Enter your name" required/>
+            </div>
+            <div className={styles.formRow}v>
+                <label>
+                    Email
+                </label>
+                <input type="email" name="email" placeholder="Enter your email address" required/>
+            </div>
+            <div className={styles.formRow}>
+                <label>
+                    Message
+                </label>
+                <textarea rows={10} name="message" placeholder="Enter your message" required/>
+            </div>
+            <button type="submit">Send</button>
+        </form>
     </div>
 }
