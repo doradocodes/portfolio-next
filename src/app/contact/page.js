7fd2e985a3cc03848b7cc7f3ff34f4478a4e804d
Dora <doradocodes@gmail.com>
2025-01-30 11:57:30 -0500

    Update form handling

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,21 @@
 import styles from "./page.module.css";
+import {useState} from "react";
 
 export default function ContactPage() {
+    const [formMessage, setFormMessage] = useState('');
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        await fetch("/__forms.html", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        });
+        // Success and error handling ...
+        setFormMessage('Message sent');
+    };
+
     return <div className="page">
         <h2>Contact me</h2>
         <div className={styles.content}>
@@ -15,7 +30,7 @@ export default function ContactPage() {
             </p>
         </div>
 
-        <form className={styles.form} data-netlify="true" name="contact">
+        <form className={styles.form} onSubmit={handleFormSubmit} name="contact">
             <div className={styles.formRow}>
                 <label>Name</label>
                 <input type="text" name="name" placeholder="Enter your name" required/>
@@ -32,6 +47,7 @@ export default function ContactPage() {
                 </label>
                 <textarea rows={10} name="message" placeholder="Enter your message" required/>
             </div>
+            <p>{formMessage}</p>
             <button type="submit">Send</button>
         </form>
     </div>
